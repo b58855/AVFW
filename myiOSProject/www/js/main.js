3b98e11ff79e028e2e50bc28917ae1630e27b67c
Evan <evan@Evans-MacBook-Pro.local>
2013-07-18 17:18:47 -0400

    instagram test 4

--- myiOSProject/www/js/main.js
+++ myiOSProject/www/js/main.js
@@ -1,10 +1,11 @@
 $('#instagram').on('pageinit', function()
 {
-	var url = "https://api.instagram.com/v1/media/popular?callback=?&amp;client_id=8143f39a078742e88debde9001247fef";
+	/*var url = "https://api.instagram.com/v1/media/popular?callback=?&amp;client_id=8143f39a078742e88debde9001247fef";
 	$.getJSON(url, function(images){
 		$.each(images.data, funtion(index, photo){
 			var item = "<li><img src='" + photo.images.standard_resolution.url + "' alte='" + photo.user.id + "' /></li>
 			$("#instagram ul").append(item);
 		});
-	});
+	});*/
+
 });
