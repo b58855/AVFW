b0c731bfab99f1d0f6e67c3e8dfdc0e97d08230c
Evan <evan@Evans-MacBook-Pro.local>
2013-07-18 17:12:23 -0400

    instagram test 2

--- myiOSProject/www/js/main.js
+++ myiOSProject/www/js/main.js
@@ -1,6 +1,6 @@
 $('#instagram').on('pageinit', function()
 {
-	var url = "https://api.instagram.com/v1/media/popular?callback=?&amp;client_id=7e4c2629226e4a5185d400a95b482edc&amp;min_id=10";
+	var url = "https://api.instagram.com/v1/media/popular?callback=?&amp;client_id=7e4c2629226e4a5185d400a95b482edc";
 	$.getJSON(url, function(images){
 		$.each(images.data, funtion(index, photo){
 			var item = "<li><img src='" + photo.images.standard_resolution.url + "' alte='" + photo.user.id + "' /></li>
